3a25e426d88259d5730784020a27f8c78508fce6
BoromyrVoitsyshen <146740149+BoromyrVoitsyshen@users.noreply.github.com>
2024-02-25 12:28:17 +0200

    Add files via upload

---
+++ LibraryMVC.Domain/LibraryMVC.Domain/Entities/BorrowedBook.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Entities
+{
+    public class BorrowedBook : Entity
+    {
+        public int bookID { get; set; }
+        public int readerID { get; set; }
+        public int borrowStart { get; set; }
+        public int borrowTime { get; set; }
+        public BorrowedBook()
+        {
+            ID = 0;
+            bookID = 0;
+            readerID = 0;
+            borrowStart = 0;
+            borrowTime = 0;
+        }
+    }
+}
---
+++ LibraryMVC.Domain/LibraryMVC.Domain/Entities/Genre.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities
+{
+    public class Genre : Entity
+    {
+        public string name { get; set; }
+        public Genre() 
+        {
+            ID = 0;
+            name = "";
+        }
+    }
+}
